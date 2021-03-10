00fe1a67b4d28b7dfae0a6413fcbcd3f4269a405
Jesus <35027233+jgalanllano@users.noreply.github.com>
2021-03-10 10:17:05 +0100

    Add data types folder

---
+++ 1-DataTypes/dataTypes.py
@@ -0,0 +1,28 @@
+# Integer. Whole numbers
+number = 3
+
+# Float. Numbers with decimal point
+floatNumber = 2.3
+
+# Strings. Ordered sequence characters
+string = "hello world!"
+
+# Booleans. Logical value indicators
+boolExp = True
+
+# Lists. Ordered sequence of objects (could be different types)
+listExp = [10, "hello", 200.3]
+
+# Dictionaries. Unordered key:value pairs
+dictExp = {"key": "value", "name": "Jesus", "age": 24}
+
+# Tuples. Ordered immutable sequence of objects
+tup = (10, "hello", 200.3)
+
+# Sets. Unordered collection of unique objects
+setExp = {"a", "b"}
+
+# Getting the data type. type() function
+
+x = 10
+print(type(x))
---
+++ 1-DataTypes/numbers.py
@@ -0,0 +1,30 @@
+# There are two main types of numbers: integers and floats
+
+# Basic arithmetics operations
+plus = 1+2
+
+subtract = 5-1
+
+multiply = 5*5
+
+divide = 15/4
+
+# Modulo operator. Return the remainder after division
+
+remainder = 7 % 4
+
+# Check if a number is even or not
+
+if 50 % 2 == 0:
+    print("Even number")
+
+else:
+    print("Odd number")
+
+# Power. 2^3
+
+powExp = 2 ** 3
+
+# Using brackets
+
+op = (2 + 10) * (10 + 3)
